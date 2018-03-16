895475f66cecf6e50d6a55d92873fd0ef18e9d74
Sidney Shiba <sidney.shiba@gmail.com>
2018-03-16 18:39:54 -0500

    Create test case for generateLocationMessage

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,8 @@
 const expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
+// Autotest for generateMessage function
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         var from = 'Sidney';
@@ -12,5 +13,21 @@ describe('generateMessage', () => {
         expect(message).toMatchObject({ from, text });
         // expect(message.from).toBe(from);
         // expect(message.text).toBe(text);
+    });
+});
+
+// Autotest for generateLocationMessage function
+describe('generateLocationMessage', () => {
+    it('should generate correct location message object', () => {
+        var from = 'Sidney';
+        var latitude = 12345;
+        var longitude = 67890;
+        var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        // expect(message.from).toBe(from);
+        // expect(message.url).toBe(url);
+        expect(message).toMatchObject({ from, url });
+        expect(typeof message.createdAt).toBe('number');
     });
 });
