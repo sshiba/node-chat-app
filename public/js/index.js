08a4650a524a712ca38e8ad6165d43a90a84eb0e
Sidney Shiba <sidney.shiba@gmail.com>
2018-03-17 15:41:10 -0500

    Using HTML template and rendering with Mustache

--- public/js/index.js
+++ public/js/index.js
@@ -12,24 +12,27 @@ socket.on('disconnect', function() {
 
 // Processing 'newMessage' event sent by server
 socket.on('newMessage', function(message) {
-    // console.log('New message', message);
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newPositionMessage', function(message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery("#location-message-template").html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
