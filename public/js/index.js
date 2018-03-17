6177b571e03f77479c8296c785364a0182b28327
Sidney Shiba <sidney.shiba@gmail.com>
2018-03-17 16:18:11 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrolltoBotton() {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 // Processing 'connect' event sent by server
 socket.on('connect', function() {
     console.log('Connected to server');
@@ -21,6 +38,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrolltoBotton();
 });
 
 socket.on('newPositionMessage', function(message) {
@@ -33,6 +51,7 @@ socket.on('newPositionMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrolltoBotton();
 });
 
 jQuery('#message-form').on('submit', function(e) {
